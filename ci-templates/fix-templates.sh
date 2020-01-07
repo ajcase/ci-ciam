cd72d75dec088cc93d6a0c536d878ec49bb5747f
Jon Harry <jonharry@uk.ibm.com>
2020-01-07 14:58:43 +0000

    Tokenize ID Sources in templates

--- ci-templates/fix-templates.sh
+++ ci-templates/fix-templates.sh
@@ -5,6 +5,7 @@ find ./templates/ -type f -name *.html -exec sed -i .bak -e 's|CI_TENANT_URL|htt
 find ./templates/ -type f -name *.html -exec sed -i .bak -e 's|DEMO_APPLICATION_URL|http://localhost:3000|g' {} \;
 find ./templates/ -type f -name *.xml -exec sed -i .bak -e 's|CI_TENANT_URL|https://yourtenantid.ice.ibmcloud.com|g' {} \;
 find ./templates/ -type f -name *.xml -exec sed -i .bak -e 's|DEMO_APPLICATION_URL|http://localhost:3000|g' {} \;
+find ./templates/ -type f -name *.html -exec sed -i .bak -e 's|GOOGLE_ID_SOURCE_ID|xxxxxxxxxxxxx|g' {} \;
+find ./templates/ -type f -name *.html -exec sed -i .bak -e 's|LINKEDIN_ID_SOURCE_ID|yyyyyyyyyyyy|g' {} \;
 zip -r ../templates.zip templates
 cd ..
-
